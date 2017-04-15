916a9d3c02e24c6bcb27823435d9398817219a87
ibrobabs <babskolawole@gmail.com>
2017-04-15 04:01:46 -0600

    Another initial

---
+++ speakout/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import home
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
+]
+
+if settings.DEBUG:
+	urlpatterns+= (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
